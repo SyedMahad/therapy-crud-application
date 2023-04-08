00b990d7fc30444fc2051a3f52e13a3e330408fd
SyedMahad <mahad.ehsan@gmail.com>
2023-04-09 01:10:55 +0500

    Refactored Patient views

--- therapycrud/therapy/serializers.py
+++ therapycrud/therapy/serializers.py
@@ -8,51 +8,16 @@ from .models import Patient, Counselor
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
 class PatientSerializer(serializers.ModelSerializer):
-    # class Meta:
-    #     model = Patient
-    #     fields = '__all__'
-    user = UserSerializer(read_only=True)
-    user_email = serializers.CharField(write_only=True, source='user.email')
 
     class Meta:
         model = Patient
-        fields = ['id', 'user', 'user_email', 'is_active']
-
-    def create(self, validated_data):
-        user_email = validated_data.pop('user')['email']
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError({'user_email': f'User with email {user_email} not found'})
-
-        patient = Patient.objects.create(user=user, **validated_data)
-
-        return patient
-
-    def update(self, instance, validated_data):
-        user_email = validated_data.pop('user')['email']
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError({'user_email': f'User with email {user_email} not found'})
-
-        instance.user = user
-        instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.save()
-
-        return instance
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.is_active = False
-        instance.save()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        fields = ['id', 'user', 'username', 'email', 'first_name', 'last_name', 'is_active']
 
 class CounselorSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
--- therapycrud/therapy/views.py
+++ therapycrud/therapy/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,6 +10,13 @@ from .serializers import PatientSerializer, CounselorSerializer
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CounselorViewSet(viewsets.ModelViewSet):
     queryset = Counselor.objects.all()
