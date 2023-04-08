bd02c35cfefb00a5df9dd2571c353b747fe765ba
SyedMahad <mahad.ehsan@gmail.com>
2023-04-09 01:19:01 +0500

    Refactored Counselor views

--- therapycrud/therapy/serializers.py
+++ therapycrud/therapy/serializers.py
@@ -20,43 +20,8 @@ class PatientSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'username', 'email', 'first_name', 'last_name', 'is_active']
 
 class CounselorSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    user_email = serializers.CharField(write_only=True, source='user.email')
 
     class Meta:
         model = Counselor
-        fields = ['id', 'user', 'user_email', 'is_active']
-
-    def create(self, validated_data):
-        user_email = validated_data.pop('user')['email']
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError({'user_email': f'User with email {user_email} not found'})
-
-        counselor = Counselor.objects.create(user=user, **validated_data)
-
-        return counselor
-
-    def update(self, instance, validated_data):
-        user_email = validated_data.pop('user')['email']
-        try:
-            user = User.objects.get(email=user_email)
-        except User.DoesNotExist:
-            raise serializers.ValidationError({'user_email': f'User with email {user_email} not found'})
-
-        instance.user = user
-        instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.save()
-
-        return instance
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.is_active = False
-        instance.save()
-
-        message = f'Counselor with id {instance.id} has been deleted successfully.'
-
-        return Response({'message': message}, status=status.HTTP_204_NO_CONTENT)
+        fields = ['id', 'user', 'username', 'email', 'first_name', 'last_name', 'is_active']
 
--- therapycrud/therapy/views.py
+++ therapycrud/therapy/views.py
@@ -1,7 +1,5 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Patient, Counselor
 from .serializers import PatientSerializer, CounselorSerializer
@@ -21,5 +19,10 @@ class PatientViewSet(viewsets.ModelViewSet):
 class CounselorViewSet(viewsets.ModelViewSet):
     queryset = Counselor.objects.all()
     serializer_class = CounselorSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
+        instance.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
